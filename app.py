6b11319c0779430b3312873cdd47a7622dfe92cb
JavierPerez76 <>
2025-02-13 09:27:19 +0100

    prueba dani

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import streamlit as st
-from dotenv import load_dotenv
-import os
 from pymongo import MongoClient
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.language.conversations import ConversationAnalysisClient
 
 def main():
     try:
-        # Cargar variables de entorno
+        # Cargar variables de entorno desde Streamlit Secrets
         ls_prediction_endpoint = st.secrets['azure_endpoint']
         ls_prediction_key = st.secrets['azure_key']
         mongodb_connection_string = st.secrets['mongodb_connection_string']
 
-        # Conectar a MongoDB
-        client = MongoClient(st.secrets["mongodb_connection_string"])
+        # Conectar a MongoDB con la connection string
+        client = MongoClient(mongodb_connection_string)  
         db = client["mongodb"]
         collection = db["computers"]
 
@@ -23,7 +21,7 @@ def main():
         user_input = st.text_input("¿Qué tipo de ordenador buscas?", "")
 
         if user_input:
-            # Crear un cliente para el modelo del servicio de lenguaje
+            # Crear un cliente para el modelo del servicio de lenguaje en Azure
             language_client = ConversationAnalysisClient(
                 ls_prediction_endpoint, AzureKeyCredential(ls_prediction_key)
             )
@@ -57,20 +55,15 @@ def main():
             top_intent = result["result"]["prediction"]["topIntent"]
             entities = result["result"]["prediction"]["entities"]
 
-            # Mostrar entidades detectadas en la terminal
-            print("Entidades detectadas:")
-            for entity in entities:
-                print(f"Categoría: {entity['category']}, Texto: {entity['text']}")
-
             # Extraer pulgadas y marca de las entidades
             pulgadas = None
             marca = None
 
             for entity in entities:
                 if entity["category"] == "pulgadas":
-                    pulgadas = str(entity["text"])  # Asegurar que sea string
+                    pulgadas = str(entity["text"])  
                 elif entity["category"] == "marca":
-                    marca = str(entity["text"])  # Asegurar que sea string
+                    marca = str(entity["text"])  
 
             # Construir la consulta para MongoDB
             query = {}
@@ -89,7 +82,7 @@ def main():
             if results:
                 st.write("Ordenadores encontrados:")
                 for doc in results:
-                    st.json(doc)  # Mostrar en formato JSON más legible
+                    st.json(doc)  
             else:
                 st.write("No se encontraron ordenadores que coincidan con tu búsqueda.")
 
