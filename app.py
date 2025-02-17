36a755bf633ee01eabacdc9ad0959dcc27db7e9a
JavierPerez76 <>
2025-02-17 09:48:05 +0100

    traducir

--- app.py
+++ app.py
@@ -1,47 +1,53 @@
-from azure.ai.language.conversations import ConversationAnalysisClient
-from azure.core.credentials import AzureKeyCredential
-from pymongo import MongoClient
-from azure.ai.translation import TextTranslationClient
 import streamlit as st
+from pymongo import MongoClient
+import requests
+import json
 import re
 
-def translate_text(text, target_language, translation_endpoint, translation_key):
+def translate_text(text, target_language, subscription_key, endpoint):
     try:
-        # Crear cliente para Azure Translator
-        translation_client = TextTranslationClient(
-            endpoint=translation_endpoint, credential=AzureKeyCredential(translation_key)
-        )
-        
-        # Preparar el texto para la traducción
-        translation_input = [{"text": text}]
-        
-        # Traducir el texto
-        response = translation_client.translate(
-            content=translation_input, 
-            target_languages=[target_language]
-        )
-        return response[0].translations[0].text
+        # Construir la URL y el cuerpo de la solicitud
+        route = f"/translate?api-version=3.0&to={target_language}"
+        url = endpoint + route
+
+        body = [{"Text": text}]
+        headers = {
+            'Ocp-Apim-Subscription-Key': subscription_key,
+            'Content-Type': 'application/json'
+        }
+
+        # Realizar la solicitud POST
+        response = requests.post(url, headers=headers, json=body)
+        response.raise_for_status()
+
+        # Obtener el texto traducido
+        response_json = response.json()
+        translated_text = response_json[0]['translations'][0]['text']
+        return translated_text
+
     except Exception as ex:
-        st.error(f"Error en la traducción: {ex}")
+        st.error(f"Error al traducir: {ex}")
         return text
 
 def parse_storage(almacenamiento):
-    try:
-        # Intentar convertir el almacenamiento a un número entero
-        return int(almacenamiento)
-    except ValueError:
-        return None
+    match = re.match(r'(\d+\.?\d*)\s*(GB|TB)', almacenamiento, re.IGNORECASE)
+    if match:
+        value = float(match.group(1))
+        unit = match.group(2).upper()
+        if unit == 'TB':
+            return int(value * 1000)  # Convertir TB a GB
+        elif unit == 'GB':
+            return int(value)
+    return None
 
 def main():
     try:
-        # Cargar variables de entorno desde Streamlit Secrets
-        ls_prediction_endpoint = st.secrets['azure_endpoint']
-        ls_prediction_key = st.secrets['azure_key']
+        # Cargar claves de la aplicación desde Streamlit Secrets
+        translation_endpoint = st.secrets['azure_endpoint']
+        translation_key = st.secrets['azure_key']
         mongodb_connection_string = st.secrets['mongodb_connection_string']
         blob_storage_url = st.secrets['blob_storage_url']
         sas_token = st.secrets['sas_token']
-        translation_endpoint = st.secrets['translation_endpoint']
-        translation_key = st.secrets['translation_key']
 
         # Conectar a MongoDB
         client = MongoClient(mongodb_connection_string)
@@ -52,12 +58,16 @@ def main():
 
         # Pedir entrada al usuario
         user_input = st.text_input("¿Qué tipo de ordenador buscas?", "")
-        target_language = st.selectbox("Selecciona un idioma para traducir:", ["Español", "Inglés", "Francés", "Chino", "Ruso"])
+        target_language = st.selectbox("Selecciona un idioma para traducir:", ["es", "en", "fr", "de"])
 
         if user_input:
-            # Crear un cliente para el modelo del servicio de lenguaje en Azure
+            # Traducir el texto del usuario antes de realizar la búsqueda
+            translated_input = translate_text(user_input, target_language, translation_key, translation_endpoint)
+            st.write(f"Texto traducido: {translated_input}")
+
+            # Crear cliente para el modelo de Azure Language
             language_client = ConversationAnalysisClient(
-                ls_prediction_endpoint, AzureKeyCredential(ls_prediction_key)
+                translation_endpoint, AzureKeyCredential(translation_key)
             )
 
             cls_project = 'CLUordenadores'
@@ -72,8 +82,8 @@ def main():
                                 "participantId": "1",
                                 "id": "1",
                                 "modality": "text",
-                                "language": "es",
-                                "text": user_input
+                                "language": target_language,
+                                "text": translated_input
                             },
                             "isLoggingEnabled": False
                         },
@@ -88,14 +98,12 @@ def main():
             top_intent = result["result"]["prediction"]["topIntent"]
             entities = result["result"]["prediction"]["entities"]
 
-            # Aquí defines cómo quieres construir la query con las entidades
-            query = {}
             pulgadas = None
             marca = None
             ram = None
             comparacion_almacenamiento = None
             almacenamiento = None
-            color = None  # Añadimos una variable para el color
+            color = None
 
             for entity in entities:
                 if entity["category"] == "Pulgadas":
@@ -110,17 +118,17 @@ def main():
                     almacenamiento = str(entity["text"]).split()[0]
                 elif entity["category"] == "ComparacionAlmacenamiento":
                     comparacion_almacenamiento = str(entity["text"]).lower()
-                elif entity["category"] == "Color":  # Detectar color
+                elif entity["category"] == "Color":
                     color = str(entity["text"]).lower()
 
-            # Construir la query
+            query = {}
             if pulgadas:
                 query["entities.Pulgadas"] = pulgadas
             if marca:
                 query["entities.Marca"] = marca
             if ram:
                 query["entities.RAM"] = ram
-            if color:  # Si hay color, agregarlo a la consulta
+            if color:
                 query["entities.Color"] = color
 
             if almacenamiento:
@@ -136,28 +144,23 @@ def main():
             results = list(collection.find(query))
 
             if results:
-                # Mostrar los resultados encontrados
                 for doc in results:
                     modelo = doc['entities'].get("Modelo", "N/A")
-                    st.subheader(modelo)  # Mostrar el modelo
+                    st.subheader(modelo)
 
+                    st.write("### Propiedades del Ordenador:")
                     detalles = []
                     for key in ["Marca", "Codigo", "Precio", "Almacenamiento", "RAM", "Pulgadas", "Procesador", "Color", "Grafica", "Garantia"]:
                         valor = doc['entities'].get(key, 'N/A')
                         if valor != 'N/A':
                             detalles.append(f"- {key}: {valor}")
+
                     st.write("\n".join(detalles))
 
-                    # Traducir el modelo y los detalles si el usuario lo seleccionó
-                    if target_language != "Español":  # Evita traducir si el idioma es español
-                        for detail in detalles:
-                            translated_detail = translate_text(detail, target_language, translation_endpoint, translation_key)
-                            st.write(translated_detail)
-
-                    # Mostrar el enlace para el PDF
                     pdf_filename = f"{doc['_id'][:-4]}.pdf"
                     pdf_url = f"{blob_storage_url}{pdf_filename}?{sas_token}"
                     st.markdown(f"[Ver PDF aquí]({pdf_url})", unsafe_allow_html=True)
+
                     st.write("---")
             else:
                 st.write("No se encontraron ordenadores que coincidan con tu búsqueda.")
